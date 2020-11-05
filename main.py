dd25b09ef4178611fc2971202156f0a7f05de383
Erik Revaj <erik.revaj3@gmail.com>
2020-11-05 21:43:22 +0100

    readline

--- main.py
+++ main.py
@@ -17,7 +17,23 @@
 #         print(line, end="")
 #         line = jabber.readline()
 
-f = open("C:\\Users\\erik\\Desktop\\[FreeCourseSite.com] Udemy - Learn Python Programming Masterclass\\"
-         "10. Input and Output (IO) in Python\\2.1 sample.txt.txt", "r")
-for line in f:
-    print(line)
+# f = open("C:\\Users\\erik\\Desktop\\[FreeCourseSite.com] Udemy - Learn Python Programming Masterclass\\"
+#          "10. Input and Output (IO) in Python\\2.1 sample.txt.txt", "r")
+# for line in f:
+#     print(line)
+#
+# f.close()
+
+# with open("C:\\Users\\erik\\Desktop\\[FreeCourseSite.com] Udemy - Learn Python Programming Masterclass\\"
+#           "10. Input and Output (IO) in Python\\2.1 sample.txt.txt", "r") as jabber:
+#     for line in jabber:
+#         if "JAB" in line.upper():
+#             print(line, end=" ")
+# # .close method is avoided
+
+with open("sample.txt", "r") as jabber:
+    lines = jabber.readline(45)
+print(lines)
+
+for line in lines[::]:
+    print(line, end="")
