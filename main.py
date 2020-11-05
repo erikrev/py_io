b0c24fd6dd22085c0e90a78ebbf662c16eeb1dbc
Erik Revaj <erik.revaj3@gmail.com>
2020-11-05 14:49:57 +0100

    added full path to sample.txt

--- main.py
+++ main.py
@@ -17,8 +17,7 @@
 #         print(line, end="")
 #         line = jabber.readline()
 
-with open("sample.txt", "r") as jabber:
-    line = jabber.readline()
-    while line:
-        print(line, end="")
-        line = jabber.readline()
+f = open("C:\\Users\\erik\\Desktop\\[FreeCourseSite.com] Udemy - Learn Python Programming Masterclass\\"
+         "10. Input and Output (IO) in Python\\2.1 sample.txt.txt", "r")
+for line in f:
+    print(line)
