f058762c8149a3b07d70f7c2c4dae311901dda60
Erik Revaj <erik.revaj3@gmail.com>
2020-11-05 02:12:11 +0100

    changed file opening

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-jabber = open("sample.txt")
+# jabber = open("sample.txt", "r")
+#
+# for line in jabber:
+#     if "dolor" in line.lower():
+#         print(line, end=" ")
+#
+# jabber.close()
 
-for line in jabber:
-    print(line)
+# with open("sample.txt", "r") as jabber:
+#     for line in jabber:
+#         if "dolor" in line.lower():
+#             print(line, end=" ")
 
-jabber.close()
+# with open("sample.txt", "r") as jabber:
+#     line = jabber.readline()
+#     while line:
+#         print(line, end="")
+#         line = jabber.readline()
+
+with open("sample.txt", "r") as jabber:
+     line = jabber.readline()
+     while line:
+         print(line, end="")
+         line = jabber.readline()
